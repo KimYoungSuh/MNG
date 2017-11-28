acb1caab4cc55515ed0ebe32928b26c7404ebfbf
정낙현 <syenart@gmail.com>
2017-11-28 12:05:46 +0900

    데스크톱용 실행파일
    데스크톱용 실행파일

--- MyNetworkGame/State/C_Mygame_state_desktop.py
+++ MyNetworkGame/State/C_Mygame_state_desktop.py
@@ -7,12 +7,6 @@ from State import C_start_state
 from State import C_collision
 import pico2d
 #
-
-if platform.architecture()[0] == '32bit':
-    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x86"
-else:
-    os.environ["PYSDL2_DLL_PATH"] = "./SDL2/x64"
-
 pico2d.open_canvas(1200,900)
 State.C_Game_framework.run(C_collision)
 pico2d.close_canvas()
